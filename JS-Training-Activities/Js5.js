d8d9b356ab0a12aef30bce57d91ae1c12d7ec848
João Vitor <joaovitorotoni6@gmail.com>
2022-10-05 15:45:41 -0300

    Js activity 5

---
+++ JS-Training-Activities/Js5.js
@@ -0,0 +1,29 @@
+//Your task is to create a function that does four basic mathematical operations.
+//The function should take three arguments - operation(string/char), value1(number), value2(number).
+//The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2)
+{
+  
+  let resultado = undefined
+  
+  switch(operation){
+      case '+':
+      resultado = value1 + value2;
+      return resultado;
+      break;
+      case '-':
+      resultado = value1 - value2;
+      return resultado;
+      break;
+      case '*':
+      resultado = value1 * value2;
+      return resultado;
+      break;
+      case '/':
+      resultado = value1 / value2;
+      return resultado;
+      break;
+     
+  }
+}
