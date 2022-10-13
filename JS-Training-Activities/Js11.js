c771e2491b2ff5117487eb646f5e06cc8530efea
João Vitor <joaovitorotoni6@gmail.com>
2022-10-13 16:36:41 -0300

    .

---
+++ JS-Training-Activities/Js11.js
@@ -0,0 +1,7 @@
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+    return num > 0 ? num * -1 : num;
+}
+
+makeNegative(45,44,66);
