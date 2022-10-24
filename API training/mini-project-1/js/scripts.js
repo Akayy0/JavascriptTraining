856504aeba4b274f93848147796f20b5736f4126
João Vitor <joaovitorotoni6@gmail.com>
2022-10-24 16:26:23 -0300

    inicio dos testes com api

---
+++ API training/mini-project-1/js/scripts.js
@@ -0,0 +1,37 @@
+// url da api
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+const loadingElement = document.querySelector("#loading")
+const postsContainer = document.querySelector("#posts-container")
+
+// get all posts
+
+async function getAllPosts(){
+
+
+    const response = await fetch(url)
+    console.log(response)
+
+    const data = await response.json();
+    console.log(data)
+
+    loadingElement.classList.add("hide");
+
+    data.map((post)=>{
+        const div = document.createElement("div")
+        const title = document.createElement("h2")
+        const body = document.createElement("p")
+        const link = document.createElement("a")
+
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "ler"
+        link.setAttribute("href",`/post.html?id=${post.id}`)
+
+        div.appendChild(title)
+        div.appendChild(body)
+        div.appendChild(link)
+    })
+}
+
+getAllPosts()
