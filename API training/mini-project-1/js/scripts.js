f40fc995e60a3a9c179acb35c747cbe5c8baac8f
João Vitor <joaovitorotoni6@gmail.com>
2022-10-25 11:27:31 -0300

    mini project 1 completo

--- API training/mini-project-1/js/scripts.js
+++ API training/mini-project-1/js/scripts.js
@@ -4,6 +4,20 @@ const url = "https://jsonplaceholder.typicode.com/posts"
 const loadingElement = document.querySelector("#loading")
 const postsContainer = document.querySelector("#posts-container")
 
+const postPage = document.querySelector("#post")
+const postContainer = document.querySelector("#post-container")
+const commentsContainer = document.querySelector("#comments-container")
+
+
+const commentForm = document.querySelector("#comment-form");
+const emailInput = document.querySelector("#email");
+const bodyInput = document.querySelector("#body");
+
+
+//get id from url
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+const postId = urlSearchParams.get("id")
 // get all posts
 
 async function getAllPosts(){
@@ -26,12 +40,95 @@ async function getAllPosts(){
         title.innerText = post.title;
         body.innerText = post.body;
         link.innerText = "ler"
-        link.setAttribute("href",`/post.html?id=${post.id}`)
+        link.setAttribute("href",`post.html?id=${post.id}`)
 
         div.appendChild(title)
         div.appendChild(body)
         div.appendChild(link)
+
+        postsContainer.appendChild(div)
     })
 }
 
-getAllPosts()
+// get individual post
+
+async function getPost(id){
+
+    const[responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`)
+    ])
+
+    const dataPost = await responsePost.json()
+    
+    const dataComments = await responseComments.json()
+
+    loadingElement.classList.add("hide")
+    postPage.classList.remove("hide")
+
+    const title = document.createElement("h1")
+    const body = document.createElement("p")
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postContainer.appendChild(title);
+    postContainer.appendChild(body);
+
+    dataComments.map((comment)=>{
+        createComment(comment)
+
+    })
+}
+
+function createComment(comment){
+    const div = document.createElement("div")
+    const email = document.createElement("h3")
+    const commentBody = document.createElement("p")
+
+    email.innerText = comment.email
+    commentBody.innerText = comment.body
+
+    div.appendChild(email);
+    div.appendChild(commentBody);
+
+    commentsContainer.appendChild(div);
+
+}
+
+//post a comment
+
+async function postComment(comment){
+    const response = await fetch(`${url}/${postId}/comments`,{
+        method: "POST",
+        body: comment,
+        headers:{
+            "Content-type": "application/json",
+        },
+    });
+
+    const data = await response.json();
+    createComment(data);
+}
+
+
+if(!postId){
+    getAllPosts();  
+}else{
+    getPost(postId);
+
+    // add event to comment form
+
+    commentForm.addEventListener("submit",(e)=>{
+        e.preventDefault();
+
+        let comment = {
+            email: emailInput.value,
+            body: bodyInput.value,
+        };
+
+        comment = JSON.stringify(comment);
+
+        postComment(comment)
+    })
+}
