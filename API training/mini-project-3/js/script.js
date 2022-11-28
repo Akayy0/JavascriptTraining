e68af7790ecf25b3802db23e44ff4671cf4f6ca3
João Vitor <joaovitorotoni6@gmail.com>
2022-11-28 11:49:34 -0300

    mini-project-3

---
+++ API training/mini-project-3/js/script.js
@@ -0,0 +1,14 @@
+const creatureIMG = document.querySelector('.creature_img')
+const creatureNAM = document.querySelector('.creature_name')
+const creatureDES = document.querySelector('.creature_description')
+const creatureLOC = document.querySelector('.creature_location')
+
+
+const fetchCreature = async(creature) =>{
+
+    const response = await fetch()
+
+
+
+
+}
