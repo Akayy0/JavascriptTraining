18c6175336f817eb87a32b55b8f6dd09b1c54748
João Vitor <joaovitorotoni6@gmail.com>
2022-11-28 19:28:24 -0300

    .

--- API training/mini-project-3/js/script.js
+++ API training/mini-project-3/js/script.js
@@ -6,9 +6,19 @@ const creatureLOC = document.querySelector('.creature_location')
 
 const fetchCreature = async(creature) =>{
 
-    const response = await fetch()
+    const response = await fetch(`https://eldenring.fanapis.com/api/creatures/:${creature}`);
+
+    if(response.status == 200){
+        const data = await APIResponse.json();
+        return data;  
+    }
+}
 
 
+const renderCreature = async(creature) =>{
+
+    const data = await fetchCreature(creature);
+    
 
 
 }
